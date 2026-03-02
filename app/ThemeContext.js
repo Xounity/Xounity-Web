0b6fa250eb86ef6d06d3324f50f5998af1e15280
0xQan <125211961+FurqanHun@users.noreply.github.com>
2026-03-02 15:45:54 +0000

    footer: remove framer motion, and use native css animations

--- app/ThemeContext.js
+++ app/ThemeContext.js
@@ -1,23 +1,24 @@
 "use client";
 
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') || 'light';
+    const savedTheme = localStorage.getItem("theme") || "light";
     setTheme(savedTheme);
-    document.body.className = savedTheme; // Set initial theme
+    document.body.classList.add(savedTheme);
   }, []);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    document.body.className = newTheme; // Update body class
+    localStorage.setItem("theme", newTheme);
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(newTheme);
   };
 
   return (
